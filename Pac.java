05b6a8bf6d48c53f3eec52ee3835d8fd79491287
mlindheimmarx <mlmc31@gmail.com>
2018-05-24 11:36:17 -0700

    Update Pac.java

--- Pac.java
+++ Pac.java
@@ -1,5 +1,5 @@
 public class Pac extends AnimatedObject {
-    
+    /* constructor */
     public Pac() {
         super(new int[][][] {{{0, 0, 1, 1, 1, 1, 1, 1, 0, 0},
                               {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -13,4 +13,15 @@ public class Pac extends AnimatedObject {
                               {1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
                               {0, 0, 1, 1, 1, 1, 1, 1, 0, 0}}});
     }
+    
+    /* methods */
+    public void nextFrame() {
+        if (currentFrameNumber != numFrames - 1) {
+            currentFrameNumber++;
+        }
+        else {
+            currentFrameNumber = 0;
+        }
+        currentState = states[currentFrameNumber];
+    } 
 }
